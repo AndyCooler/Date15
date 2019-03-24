3e3ff558ce29c589f56d42abc8c260f484862a08
Andreas Kohler <andreas.kohler.76@googlemail.com>
2019-03-24 22:01:57 +0100

    refactoring: extract creation of notification to separate class

---
+++ app/src/main/java/com/mythosapps/date15/NotificationBuilder.java
@@ -0,0 +1,81 @@
+package com.mythosapps.date15;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+public class NotificationBuilder {
+
+    static final List<Integer> dateIcons = new ArrayList<>();
+    static {
+        dateIcons.add(R.drawable.ic_date_1);
+        dateIcons.add(R.drawable.ic_date_2);
+        dateIcons.add(R.drawable.ic_date_3);
+        dateIcons.add(R.drawable.ic_date_4);
+        dateIcons.add(R.drawable.ic_date_5);
+        dateIcons.add(R.drawable.ic_date_6);
+        dateIcons.add(R.drawable.ic_date_7);
+        dateIcons.add(R.drawable.ic_date_8);
+        dateIcons.add(R.drawable.ic_date_9);
+        dateIcons.add(R.drawable.ic_date_10);
+        dateIcons.add(R.drawable.ic_date_11);
+        dateIcons.add(R.drawable.ic_date_12);
+        dateIcons.add(R.drawable.ic_date_13);
+        dateIcons.add(R.drawable.ic_date_14);
+        dateIcons.add(R.drawable.ic_date_15);
+        dateIcons.add(R.drawable.ic_date_16);
+        dateIcons.add(R.drawable.ic_date_17);
+        dateIcons.add(R.drawable.ic_date_18);
+        dateIcons.add(R.drawable.ic_date_19);
+        dateIcons.add(R.drawable.ic_date_20);
+        dateIcons.add(R.drawable.ic_date_21);
+        dateIcons.add(R.drawable.ic_date_22);
+        dateIcons.add(R.drawable.ic_date_23);
+        dateIcons.add(R.drawable.ic_date_24);
+        dateIcons.add(R.drawable.ic_date_25);
+        dateIcons.add(R.drawable.ic_date_26);
+        dateIcons.add(R.drawable.ic_date_27);
+        dateIcons.add(R.drawable.ic_date_28);
+        dateIcons.add(R.drawable.ic_date_29);
+        dateIcons.add(R.drawable.ic_date_30);
+        dateIcons.add(R.drawable.ic_date_31);
+    }
+
+    public static Notification buildForToday(Context context) {
+
+        Calendar cal = GregorianCalendar.getInstance();
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int todaysDateIcon = dateIcons.get(day-1); // index zero-based
+        Toast.makeText(context,"On: " + day,Toast.LENGTH_SHORT).show();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, Context.NOTIFICATION_SERVICE)
+                .setSmallIcon(todaysDateIcon)
+                .setContentTitle("Date in Status Bar")
+                .setContentText(""+day)
+                .setOnlyAlertOnce(true)
+                //.setShowWhen(false)
+                //.setExtras()
+                .setOngoing(true)
+                .setVisibility(NotificationCompat.VISIBILITY_SECRET)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // ab Android Oreo mit Channel:
+            NotificationChannel channel = new NotificationChannel("date15_channel", "date15_channel", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager nMgr = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            nMgr.createNotificationChannel(channel);
+            builder.setChannelId("date15_channel");
+        }
+        return builder.build();
+    }
+}
--- app/src/main/java/com/mythosapps/date15/SettingsActivity.java
+++ app/src/main/java/com/mythosapps/date15/SettingsActivity.java
@@ -211,40 +211,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
     public static class NotificationPreferenceFragment extends PreferenceFragment {
 
         Notification notification;
-        static final List<Integer> dateIcons = new ArrayList<>();
-        static {
-            dateIcons.add(R.drawable.ic_date_1);
-            dateIcons.add(R.drawable.ic_date_2);
-            dateIcons.add(R.drawable.ic_date_3);
-            dateIcons.add(R.drawable.ic_date_4);
-            dateIcons.add(R.drawable.ic_date_5);
-            dateIcons.add(R.drawable.ic_date_6);
-            dateIcons.add(R.drawable.ic_date_7);
-            dateIcons.add(R.drawable.ic_date_8);
-            dateIcons.add(R.drawable.ic_date_9);
-            dateIcons.add(R.drawable.ic_date_10);
-            dateIcons.add(R.drawable.ic_date_11);
-            dateIcons.add(R.drawable.ic_date_12);
-            dateIcons.add(R.drawable.ic_date_13);
-            dateIcons.add(R.drawable.ic_date_14);
-            dateIcons.add(R.drawable.ic_date_15);
-            dateIcons.add(R.drawable.ic_date_16);
-            dateIcons.add(R.drawable.ic_date_17);
-            dateIcons.add(R.drawable.ic_date_18);
-            dateIcons.add(R.drawable.ic_date_19);
-            dateIcons.add(R.drawable.ic_date_20);
-            dateIcons.add(R.drawable.ic_date_21);
-            dateIcons.add(R.drawable.ic_date_22);
-            dateIcons.add(R.drawable.ic_date_23);
-            dateIcons.add(R.drawable.ic_date_24);
-            dateIcons.add(R.drawable.ic_date_25);
-            dateIcons.add(R.drawable.ic_date_26);
-            dateIcons.add(R.drawable.ic_date_27);
-            dateIcons.add(R.drawable.ic_date_28);
-            dateIcons.add(R.drawable.ic_date_29);
-            dateIcons.add(R.drawable.ic_date_30);
-            dateIcons.add(R.drawable.ic_date_31);
-        }
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
@@ -279,34 +245,9 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
                         // Unchecked the switch programmatically
                         onOffDateNotification.setChecked(true);
 
-                        Calendar cal = GregorianCalendar.getInstance();
-                        int day = cal.get(Calendar.DAY_OF_MONTH);
-                        int todaysDateIcon = dateIcons.get(day-1); // index zero-based
-                        Toast.makeText(getContext(),"On: " + day,Toast.LENGTH_SHORT).show();
-
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), NOTIFICATION_SERVICE)
-                                .setSmallIcon(todaysDateIcon)
-                                .setContentTitle("Date in Status Bar")
-                                .setContentText(""+day)
-                                .setOnlyAlertOnce(true)
-                                //.setShowWhen(false)
-                                //.setExtras()
-                                .setOngoing(true)
-                                .setVisibility(NotificationCompat.VISIBILITY_SECRET)
-                                .setPriority(NotificationCompat.PRIORITY_HIGH);
-
-
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            // ab Android Oreo mit Channel:
-                            NotificationChannel channel = new NotificationChannel("date15_channel", "date15_channel", NotificationManager.IMPORTANCE_HIGH);
-                            NotificationManager nMgr = (NotificationManager) getContext().getSystemService(Context.NOTIFICATION_SERVICE);
-                            nMgr.createNotificationChannel(channel);
-                            builder.setChannelId("date15_channel");
-                        }
+                        notification = NotificationBuilder.buildForToday(getContext());
 
                         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getContext());
-
-                        notification = builder.build();
                         notificationManager.notify(4711, notification);
 
                     }
